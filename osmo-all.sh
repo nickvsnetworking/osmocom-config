39a27744165000996f4a8708cb01b9aa6be5625a
Nick <nick@nickvsnetworking.com>
2020-05-23 19:42:55 +1000

    Added GPRS / EDGE Config

--- osmo-all.sh
+++ osmo-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cmd="${1:-status}" 
 set -ex
-systemctl $cmd osmo-hlr osmo-msc osmo-mgw osmo-stp osmo-bsc 
+systemctl $cmd osmo-hlr osmo-msc osmo-mgw osmo-stp osmo-bsc osmo-ggsn osmo-sgsn 
